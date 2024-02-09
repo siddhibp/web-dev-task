fdbf6b5cfce19e6c0359a7efb8b1d1ee8b9b7229
Siddhi Panchabhai <siddhipanchabhai@gmail.com>
2024-02-09 21:51:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Services from './components/services';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Services/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/services.jsx
@@ -0,0 +1,267 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+function Services () {
+    let message = `Welcome to [Your company name] where a dedicated team of professional works \n together to bring innovation, creativity and expertise to software industry get to know \n the individuals who make our company thrive.`;
+    return (
+      <section class="section-white">
+ 
+    <div class="container">
+ 
+        <div class="row">
+       
+                    <div class="col-md-12 text-center">
+                    <button className="section-button">Company Name</button>
+                    <p className="section-subtitle center">{message}</p>
+                    <p className="section-subtitlea centera"><b>---------------------------------------------------------------------</b></p>    
+                    </div> <br></br>
+                    
+
+                    <div class="col-md-12 text-center">
+                    <h2 class="section-title center">Meet our Team</h2>
+                    <button className="section-buttona">Leadership/ Management Team</button>
+                    </div> 
+        </div>
+    </div>
+
+             
+
+ 
+
+              
+<div class="containera">
+    <div class="rowa">
+        <div class="col-custom-sm-6 col-custom-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                <h3>[Founder Name]</h3>            
+                    <div class="team-info"><p>Founder/ CEO</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-6 col-custom-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                    <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Founder Name]</h3>            
+                    <div class="team-info"><p>Founder/ CEO</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+
+
+
+
+
+
+<div class="containerb">
+    <div class="rowb">
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[VP Name]</h3>            
+                    <div class="team-info"><p>VP, Development &<br></br> Growth</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[VP Name]</h3>            
+                    <div class="team-info"><p>VP, Sales &<br></br> Marketing</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[VP Name]</h3>            
+                    <div class="team-info"><p>VP, Finance &<br></br> Operation</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+         
+
+
+<div class="container">
+        <div class="row">       
+             <button className="section-buttona">Development Team</button>
+        </div>
+</div>
+
+
+<div class="containerb">
+    <div class="rowb">
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Team Lead SE</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+         
+
+
+<div class="containerb">
+    <div class="rowb">
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Developer Name]</h3>            
+                    <div class="team-info"><p>Software Engineer</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+
+
+<div class="container">
+        <div class="row">       
+             <button className="section-buttona">Sales and Marketing Team</button>
+        </div>
+</div>
+
+
+
+
+<div class="containerb">
+    <div class="rowb">
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Employee Name]</h3>            
+                    <div class="team-info"><p>Sales Officer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Employee Name]</h3>            
+                    <div class="team-info"><p>Sales Officer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Employee Name]</h3>            
+                    <div class="team-info"><p>Sales Officer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-custom-sm-3 col-custom-md-3">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Employee Name]</h3>            
+                    <div class="team-info"><p>Sales Officer</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+         
+
+
+<div class="containerb">
+    <div class="rowb">
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Founder Name]</h3>              
+                    <div class="team-info"><p>Marketing Officer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Founder Name]</h3>            
+                    <div class="team-info"><p>Marketing Officer</p></div>
+                </div>
+            </div>
+        </div> 
+        <div class="col-sm-4 col-md-4">
+            <div class="team-item">
+                <div class="member-info">
+                <img src="https://img.lovepik.com/free-png/20210926/lovepik-cartoon-customer-service-avatar-png-image_401451024_wh1200.png" class="team-img" alt="pic" />                   
+                    <h3>[Founder Name]</h3>            
+                    <div class="team-info"><p>Marketing Officer</p></div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+
+
+
+    </section>
+    )
+}
+
+export default Services;
